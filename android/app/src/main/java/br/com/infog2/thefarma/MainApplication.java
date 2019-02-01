d531534622e01d62ac581aed24c61fc0fba7ed5e
Weslley de Sousa <weslleyocara@hotmail.com>
2019-02-01 19:05:10 -0300

    Firebase

--- android/app/src/main/java/br/com/infog2/thefarma/MainApplication.java
+++ android/app/src/main/java/br/com/infog2/thefarma/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 
-//import com.evollu.react.fcm.FIRMessagingPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;                       
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
@@ -49,7 +48,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            //new FIRMessagingPackage(),
             new RNFirebasePackage(),
             new RNFirebaseMessagingPackage(),
             new RNFirebaseNotificationsPackage(),
